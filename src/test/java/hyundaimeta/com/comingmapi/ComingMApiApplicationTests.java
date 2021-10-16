1a37eb22459e1d5d2a5d8a2d3bcc2e2a1fff3d1b
에단(손형규) <59630175+gudrb33333@users.noreply.github.com>
2021-10-16 12:06:38 +0900

    2021-10-16 에단

--- src/test/java/hyundaimeta/com/comingmapi/ComingMApiApplicationTests.java
+++ src/test/java/hyundaimeta/com/comingmapi/ComingMApiApplicationTests.java
@@ -1,13 +1,10 @@
 package hyundaimeta.com.comingmapi;
 
-import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ComingMApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
--- src/test/java/hyundaimeta/com/comingmapi/apis/healthCheck/HealthCheckControllerTest.java
+++ src/test/java/hyundaimeta/com/comingmapi/apis/healthCheck/HealthCheckControllerTest.java
@@ -1,41 +1,6 @@
 package hyundaimeta.com.comingmapi.apis.healthCheck;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-
-import org.springframework.restdocs.RestDocumentationExtension;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import hyundaimeta.com.comingmapi.apis.healthCheck.controller.HealthCheckController;
-
-@ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
-@WebMvcTest(HealthCheckController.class)
-@AutoConfigureRestDocs
 public class HealthCheckControllerTest {
 	
-	@Autowired
-    private MockMvc mockMvc;
-	
-    @Autowired
-    private ObjectMapper objectMapper;
-    
-    @Test
-    @DisplayName("컨택하기")
-    void healthCheck() throws Exception{
-  
-   
-    }
+
 }

071c2727bbeb212bdfe828ed2c84cef5f7548b43
에단(손형규) <59630175+gudrb33333@users.noreply.github.com>
2021-10-18 23:39:39 +0900

    2021-10-18 에단

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ echo 'start ./start.sh'
 
 set -xe \
 && sudo rm -rf /home/ec2-user/coming-m-api/* \
-&& mkdir /home/ec2-user/coming-m-api/build
-&& mkdir /home/ec2-user/coming-m-api/build/libs
+&& mkdir /home/ec2-user/coming-m-api/build \
+&& mkdir /home/ec2-user/coming-m-api/build/libs \
 && cp -rf /home/ec2-user/ComingMApiTemp/* /home/ec2-user/coming-m-api/build/libs \
 && sudo service coming-m-api start
